dc7855706ff61cc4c0045004a8b88f8e8bc1b071
Datta Sutar <sutardattatray123@gmail.com>
2025-09-14 12:29:18 +0530

    TC_RF_007

---
+++ src/test/java/tutorialninja/register/TC_RF_007.java
@@ -0,0 +1,40 @@
+package tutorialninja.register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_RF_007 {
+	
+@Test
+	public void verifyNavigatingRegisterAccountpageUsingDifferentWays() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/");
+		
+		//Verify navigating on register page
+		driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
+		//1)By clicking  on register
+		driver.findElement(By.linkText("Register")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Register']")).isDisplayed());
+		
+		//2)verify by clicking on login 
+		driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
+		driver.findElement(By.linkText("Login")).click();
+		driver.findElement(By.xpath("//a[normalize-space()='Continue']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Register']")).isDisplayed());
+
+		//3)By clicking on right side menu on register
+		driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
+		driver.findElement(By.linkText("Login")).click();
+		driver.findElement(By.xpath("//a[@class='list-group-item'][normalize-space()='Register']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Register']")).isDisplayed());
+
+	}
+}
