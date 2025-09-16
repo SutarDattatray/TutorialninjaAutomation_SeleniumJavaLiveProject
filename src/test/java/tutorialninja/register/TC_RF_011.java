e6024a9bca11a7c83b5c0c0ca09025369882d2a7
Datta Sutar <sutardattatray123@gmail.com>
2025-09-16 13:39:34 +0530

    TC_RF_011

---
+++ src/test/java/tutorialninja/register/TC_RF_011.java
@@ -0,0 +1,63 @@
+package tutorialninja.register;
+
+import java.time.Duration;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class TC_RF_011 {
+	WebDriver driver;
+	@AfterMethod
+	public void  tearDown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void verifyRegisterAccountWithInvalidPgoneNumber() {
+
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/");
+
+		// Click on myaccount dropmenu
+		driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
+		// click on register
+		driver.findElement(By.linkText("Register")).click();
+
+		// 1)fill mandatory field in register form
+		driver.findElement(By.id("input-firstname")).sendKeys("Dattatray");
+		driver.findElement(By.id("input-lastname")).sendKeys("Sutar");
+		driver.findElement(By.id("input-email")).sendKeys(generateNewEmail());
+		driver.findElement(By.id("input-telephone")).sendKeys("abcgds");
+		driver.findElement(By.id("input-password")).sendKeys("Sutar@123");
+		driver.findElement(By.id("input-confirm")).sendKeys("Sutar@123");
+		driver.findElement(By.name("agree")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+
+		String expectedwarningmessage = "Telephone  umber does not appear  to be valid";
+		Assert.assertEquals(
+				driver.findElement(By.xpath("//input[@id='input-telephone']/following-sibling::div")).getText(),
+				expectedwarningmessage);
+
+		
+	}
+
+	@Test
+	public String generateNewEmail() {
+		return new Date().toString().replaceAll("\\s", "").replaceAll("\\:", "") + "@gmail.com";
+
+		/*
+		 * Date date = new Date(); String dateString = date.toString(); String
+		 * noSpaceDateString = dateString.replaceAll("\\s", ""); String
+		 * noSpaceAndNoColonDateString = noSpaceDateString.replaceAll("\\:", ""); String
+		 * emailWithTimeStamp = noSpaceAndNoColonDateString+"@gmail.com"; return
+		 * emailWithTimeStamp;
+		 */
+	}
+}
--- src/test/java/tutorialninja/register/TC_RF_01_10.java
+++ src/test/java/tutorialninja/register/TC_RF_01_10.java
@@ -18,12 +18,11 @@ import org.testng.annotations.Test;
 import ru.yandex.qatools.ashot.comparison.ImageDiff;
 import ru.yandex.qatools.ashot.comparison.ImageDiffer;
 
-public class TC_RF_010 {
-	WebDriver driver;
+public class TC_RF_01_10 {
 
 	@Test
 	public void verifyRegisterAccountWithInvalidEmail() throws InterruptedException, IOException {
-		driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
 		driver.get("https://tutorialsninja.com/demo/");
@@ -33,10 +32,9 @@ public class TC_RF_010 {
 		// click on register
 		driver.findElement(By.linkText("Register")).click();
 
+		// 1)fill mandatory field in register form
 		driver.findElement(By.id("input-firstname")).sendKeys("Dattatray");
 		driver.findElement(By.id("input-lastname")).sendKeys("Sutar");
-
-		// case 1:Verify by providing invalid email:-1223444
 		driver.findElement(By.id("input-email")).sendKeys("1223444");
 		driver.findElement(By.id("input-telephone")).sendKeys("9145488096");
 		driver.findElement(By.id("input-password")).sendKeys("Sutar@123");
@@ -47,43 +45,24 @@ public class TC_RF_010 {
 
 		Thread.sleep(5000);
 
-		// case 1
 		File srcfile1 = driver.findElement(By.xpath("//form[@class='form-horizontal']"))
 				.getScreenshotAs(OutputType.FILE);
+
 		FileHandler.copy(srcfile1, new File(System.getProperty("user.dir") + "\\Screenshots\\sc1Actual.png"));
 
-		Thread.sleep(5000);
 		// used for compare two screenshots
-				BufferedImage ActualBImage = ImageIO
-						.read(new File(System.getProperty("user.dir") + "\\Screenshots\\sc1Actual.png"));
+		BufferedImage ActualBImage = ImageIO
+				.read(new File(System.getProperty("user.dir") + "\\Screenshots\\sc1Actual.png"));
 
-				BufferedImage ExpectedBImage = ImageIO
-						.read(new File(System.getProperty("user.dir") + "\\Screenshots\\sc1Expected.png"));
+		BufferedImage ExpectedBImage = ImageIO
+				.read(new File(System.getProperty("user.dir") + "\\Screenshots\\sc1Expected.png"));
 
-				ImageDiffer imgdiffer = new ImageDiffer();
-				ImageDiff imgDifference = imgdiffer.makeDiff(ExpectedBImage, ActualBImage);
-				Assert.assertFalse(imgDifference.hasDiff());
-				
-				driver.findElement(By.id("input-email")).clear();
-				
-				/*driver.findElement(By.id("input-email")).sendKeys("sutardattatray@");
-				driver.findElement(By.xpath("//input[@value='Continue']")).click();
-				
-				File srcfile2 = driver.findElement(By.xpath("//form[@class='form-horizontal']"))
-						.getScreenshotAs(OutputType.FILE);
-				FileHandler.copy(srcfile2, new File(System.getProperty("user.dir") + "\\Screenshots\\sc2Actual.png"));
+		ImageDiffer imgdiffer = new ImageDiffer();
+		ImageDiff imgDifference = imgdiffer.makeDiff(ExpectedBImage, ActualBImage);
+		Assert.assertFalse(imgDifference.hasDiff());
 
-				/*BufferedImage ActualBImage1 = ImageIO
-						.read(new File(System.getProperty("user.dir") + "\\Screenshots\\sc2Actual.png"));
+		driver.quit();
+	}
 
-				BufferedImage ExpectedBImage1 = ImageIO
-						.read(new File(System.getProperty("user.dir") + "\\Screenshots\\sc2Expected.png"));
+}
 
-				ImageDiffer imgdiffer1 = new ImageDiffer();
-				ImageDiff imgDifference1 = imgdiffer.makeDiff(ExpectedBImage, ActualBImage);
-				Assert.assertFalse(imgDifference1.hasDiff());*/
-				
-				driver.quit();
-		
-
-	}}
