32fcf8ab127fa36bf9275fefd753ff90b398bc1b
Datta Sutar <sutardattatray123@gmail.com>
2025-09-16 15:01:07 +0530

    TC_RF_013

---
+++ src/test/java/tutorialninja/register/TC_RF_013.java
@@ -0,0 +1,52 @@
+package tutorialninja.register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_RF_013 {
+@Test
+	public void verifyPlaceHolderOfTextFieldsInregisterAccountPage() {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/");
+
+		driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+
+		String expectedFirstnamePlaceHolderText = "First Name";
+		Assert.assertEquals(driver.findElement(By.id("input-firstname")).getAttribute("placeholder"),
+				expectedFirstnamePlaceHolderText);
+		
+		String expectedLastnamePlaceHolderText = "Last Name";
+		Assert.assertEquals(driver.findElement(By.id("input-lastname")).getAttribute("placeholder"),
+				expectedLastnamePlaceHolderText);
+		
+		String expectedEmailPlaceHolderText = "E-Mail";
+		Assert.assertEquals(driver.findElement(By.id("input-email")).getAttribute("placeholder"),
+				expectedEmailPlaceHolderText);
+
+		String expectedTelephonePlaceHolderText = "Telephone";
+		Assert.assertEquals(driver.findElement(By.id("input-telephone")).getAttribute("placeholder"),
+				expectedTelephonePlaceHolderText);
+		
+		String expectedPasswordPlaceHolderText = "Password";
+		Assert.assertEquals(driver.findElement(By.id("input-password")).getAttribute("placeholder"),
+				expectedPasswordPlaceHolderText);
+		
+		String expectedPasswordConfirmPlaceHolderText = "Password Confirm";
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-confirm']")).getAttribute("placeholder"),
+				expectedPasswordConfirmPlaceHolderText);
+		
+		driver.quit();
+
+
+
+	}
+}
